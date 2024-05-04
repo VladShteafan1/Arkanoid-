e7d597209191907b7dd873af39cb65c1c12f18ea
VladShtefan1 <shtefanvlad@knu.ua>
2024-05-04 12:46:24 +0300

    add coments in bonus.py

--- bonus.py
+++ bonus.py
@@ -3,9 +3,15 @@ from random import choice
 
 class Bonus:
     def __init__(self, x, y):
+        """
+            Ініціалізує об'єкт бонусу з вказанням його розмірів та позиції
+        """
         self.width = 20
         self.height = 20
         self.rect = pygame.Rect(x - self.width // 2, y - self.height // 2, self.width, self.height)
         self.type = choice(["speed_up", "paddle_extension", "ball_speed_decrease", "paddle_size_decrease"])
     def move(self):
+        """
+            Зміщує бонус вниз по екрану.
+        """
         self.rect.y += 5
